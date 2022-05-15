fa9200c2b3759aefe8d5f6ca07e1219feeea52ba
Alisa Biclesanu <39887025+biclesanu@users.noreply.github.com>
2022-05-16 02:35:16 +0300

    Update nema17.ino

--- nema17.ino
+++ nema17.ino
@@ -8,7 +8,7 @@
 #define stepPinox 3
 #define dirPinoy 4
 #define stepPinoy 5
-#define stepsPerRevolution 200
+#define stepsPerRevolution 500
 #define SS_PIN 53
 #define RST_PIN 46
 
@@ -35,11 +35,11 @@ int valn1=0, valn2=0, valn3=0, valn4=0;
 int led=35;
 int deridicat=0;
 int obiectecastigate=0;
+int oxpozitie=0;
+int oypozitie=0;
 double ozpozitie=0;
-double oxpozitie=0;
-double oypozitie=0;
 
-int potpin =2; // pin analog conectare potentiometru
+int potpin =8; // pin analog conectare potentiometru
 int Pin0 = 8; //pini pt stepper5v sus-jos
 int Pin1 = 9; 
 int Pin2 = 10; 
@@ -57,6 +57,25 @@ const int d4 =45;
 const int d3 =44;
 const int d2 =43;
 const int d1 =42;
+
+#define OCTAVE_OFFSET 0
+#define isdigit(n) (n >= '0' && n <= '9')
+
+const int tonePin = 24;
+
+int notes[] = { 0,
+                262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494,
+                523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988,
+                1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976,
+                2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951
+              };
+
+char *piesaintro = "FinalFan:d=4,o=5,b=112:16a4,16a4,16a4,8c.,16a4,16a4,16a4,8d.,16a4,16a4,16a4,16d#,16d,16c,16d,16c,16b4,8c,16b4,16a4,16a4,16a4,8c.,16a4,16a4,16a4,8d.,16a4,16a4,16a4,16d#,16d,16c,16d,16c,16b4,8c,16b4,16c,16c,16c,8d#.,16c,16c,16c,8f.,16c,16c,16c,16f#,16f,16d#,16f,16d#,16d,8d#,16d,16c,16c,16c,8d#.,16c,16c,16c,8f.,16c,16c,16c,16f#,16f,16d#,16f,16d#,16d,16d#,16d,16c";
+char *piesaapasat= "ChocoboT:d=4,o=6,b=160:32c5,32d5,32e5,32f5,32g5,32a5,32b5,32c,32d,32e,32f,32g,32a,32b,32c7,16d7,8p,8b,8g,8e,8d7,8b,8g,8b,8p,8g,8p,b,8p";
+char *piesacazut = "DuHast:d=4,o=6,b=160:16a,16g,16d,16p,16f,16p,16e,16p,8a5,16d,16f,16g,16p,16e,8p,16a,16g,16d,16f,16e,16a5,16e,16f,16g,8e";
+char *piesafinal = "SuperMar:d=16,o=6,b=112:e,32p,8e,c,8e,8g,8g5,8c,p,8g5,p,8e5,p,8a5,8b5,a#5,8a5,g.5,e,g,8a,f,8g,8e,c,d,8b5,p,8c,p,8g5,p,8e5,p,8a5,8b5,a#5,8a5,g.5,e,g,8a,f,8g,8e";
+char *piesacredit = "Theme:d=4,o=5,b=112:32f#6,32g6,32p,32g6,32g#6,32a6,32p,32b.6";
+
 
 byte t[8] =   
 {
@@ -110,8 +129,8 @@ void setup() {
   Serial.begin(9600);
   myStepper.setSpeed(100);
   myservo.attach(22);
-  SPI.begin(); // Initiate SPI bus
-  mfrc522.PCD_Init(); // Initiate MFRC522
+  SPI.begin();
+  mfrc522.PCD_Init(); 
   pinMode(d1, OUTPUT);
   pinMode(d2, OUTPUT);
   pinMode(d3, OUTPUT);
@@ -151,6 +170,7 @@ void setup() {
   lcd.createChar(4,a2);
   lcd.createChar(5,i);
   lcd.createChar(7,a1);
+  digitalWrite(35,HIGH);
 }
 
 //LCD
@@ -359,6 +379,8 @@ void pagfelicitari(){
   lcd.setCursor(4,1);
   lcd.print("+1 obiecte");
 
+  play_rtttl(piesacazut);
+
 }
 
 void mesajfinal(){
@@ -377,13 +399,10 @@ void mesajfinal(){
   if(obiectecastigate==0){
     
   lcd.setCursor(0,0);
-  lcd.print("Nu ai castigat obiecte");
+  lcd.print("Nu ai castigat:(");
   lcd.setCursor(0,1);
   lcd.print("Mai incearca!");
   }
-
-
-  delay(9000);
 }
 
 void switcher(){
@@ -631,12 +650,13 @@ void nine()
 
 //Senzori cadere
 void ajustaresenzorisimediesenzori(){
+  delay(100);
   valn1=0;
   valn2=0;
   valn3=0;
   valn4=0;
   for(int i=1;i<=5;i++){
-    delay(100);
+    delay(90);
     valn1=valn1+analogRead(sen1);
     valn2=valn2+analogRead(sen2);
     valn3=valn3+analogRead(sen3);
@@ -646,58 +666,197 @@ void ajustaresenzorisimediesenzori(){
   valn2=valn2/5;
   valn3=valn3/5;
   valn4=valn4/5;
-   delay(50);
 }
 
 int cadere(){
 
-  if(analogRead(sen1)+60<=valn1)
+  if(analogRead(sen1)+100<=valn1)
     return 1;
 
-  if(analogRead(sen2)+60<=valn2)
+  if(analogRead(sen2)+100<=valn2)
     return 1;
 
-  if(analogRead(sen3)+60<=valn3)
+  if(analogRead(sen3)+100<=valn3)
     return 1;
   
-  if(analogRead(sen4)+50<=valn4)
+  if(analogRead(sen4)+100<=valn4)
     return 1;
   
    return 0;
 }
 
 //Buzzer
-/*
-void canteccadere(){
+
+void play_rtttl(char *p)
+{
+  byte default_dur = 4;
+  byte default_oct = 6;
+  int bpm = 63;
+  int num;
+  long wholenote;
+  long duration;
+  byte note;
+  byte scale;
+
+  while (*p!=':') p++; 
+  p++;                
+
+  if (*p == 'd')
+  {
+    p++; p++;       
+    num = 0;
+    while (isdigit(*p))
+    {
+      num = (num * 10) + (*p++ - '0');
+    }
+    if (num > 0) default_dur = num;
+    p++;                  
+  }
+
+  // get default octave
+  if (*p == 'o')
+  {
+    p++; p++;              
+    num = *p++ - '0';
+    if (num >= 3 && num <= 7) default_oct = num;
+    p++;                   
+  }
+
+  // get BPM
+  if (*p == 'b')
+  {
+    p++; p++;             
+    num = 0;
+    while (isdigit(*p))
+    {
+      num = (num * 10) + (*p++ - '0');
+    }
+    bpm = num;
+    p++;                  
+  }
+
+  wholenote = (60 * 1000L / bpm) * 4;  
+
+
+  while (*p)
+  {
+
+    num = 0;
+    while (isdigit(*p))
+    {
+      num = (num * 10) + (*p++ - '0');
+    }
+
+    if (num) duration = wholenote / num;
+    else duration = wholenote / default_dur;  
+
+    note = 0;
+
+    switch (*p)
+    {
+      case 'c':
+        note = 1;
+        break;
+      case 'd':
+        note = 3;
+        break;
+      case 'e':
+        note = 5;
+        break;
+      case 'f':
+        note = 6;
+        break;
+      case 'g':
+        note = 8;
+        break;
+      case 'a':
+        note = 10;
+        break;
+      case 'b':
+        note = 12;
+        break;
+      case 'p':
+      default:
+        note = 0;
+    }
+    p++;
+
+    
+    if (*p == '#')
+    {
+      note++;
+      p++;
+    }
+
+    
+    if (*p == '.')
+    {
+      duration += duration / 2;
+      p++;
+    }
+
+    
+    if (isdigit(*p))
+    {
+      scale = *p - '0';
+      p++;
+    }
+    else
+    {
+      scale = default_oct;
+    }
+
+    scale += OCTAVE_OFFSET;
+
+    if (*p == ',')
+      p++;       
+      
+    if (note)
+    {
+      tone(tonePin, notes[(scale - 4) * 12 + note]);
+      delay(duration);
+      noTone(tonePin);
+    }
+    else
+    {
+      delay(duration);
+    }
+  }
 }
 
-void cantecfinal(){  ///cantec cand se afiseaza scorul final
-}
 
-void cantecinceput(){
-}*/
 
-//X Y Z
 void oxmovement()
 {
-   for (int i = 0; i < stepsPerRevolution; i++) {
-    digitalWrite(stepPinox, HIGH);
-    delayMicroseconds(1000);
+   for (int i = 0; i < 100; i++) {
+   digitalWrite(stepPinox, HIGH);
+    delayMicroseconds(2000);
     digitalWrite(stepPinox, LOW);
-    delayMicroseconds(1000);  }
+    delayMicroseconds(450);  }
     
   
 }
 
 void oymovement()
 {
-   for (int i = 0; i < stepsPerRevolution; i++) {
+   for (int i = 0; i < 100; i++) {
     digitalWrite(stepPinoy, HIGH);
-    delayMicroseconds(1000);
+    delayMicroseconds(2000);
     digitalWrite(stepPinoy, LOW);
-    delayMicroseconds(1000);}
+    delayMicroseconds(450);}
+ 
  
   
+}
+
+void repozitionarexoy(){
+
+  while(!(oxpozitie==0))
+      digitalWrite(dirPinox, LOW),oxpozitie=oxpozitie-1,oxmovement();
+
+  while(!(oypozitie==0))
+      digitalWrite(dirPinoy, HIGH),oypozitie=oypozitie-1,oymovement();
+
 }
 
 void oxoy()
@@ -705,18 +864,19 @@ void oxoy()
   OX=analogRead(A0);
   OY=analogRead(A1);
 
-  if(OX<200)
-    digitalWrite(dirPinox, HIGH), oxmovement();
+  if(OX<50 && oxpozitie<12)
+    digitalWrite(dirPinox, HIGH), oxpozitie=oxpozitie+1, oxmovement();
 
-  if(OX>800)
-    digitalWrite(dirPinox, LOW),oxmovement();
+  if(OX>900 && oxpozitie>0)
+    digitalWrite(dirPinox, LOW),oxpozitie=oxpozitie-1,oxmovement();
 
-  if(OY<200)
-    digitalWrite(dirPinoy, HIGH),oymovement();
+  if(OY<50 && oypozitie>0)
+    digitalWrite(dirPinoy, HIGH),oypozitie=oypozitie-1,oymovement();
    
-  if(OY>800)
-  digitalWrite(dirPinoy, LOW),oymovement();
+  if(OY>900 && oypozitie<16)
+  digitalWrite(dirPinoy, LOW),oypozitie=oypozitie+1,oymovement();
 }
+
 
 void clockwise()
 {
@@ -864,33 +1024,34 @@ default:
 
 void oz()
 {
-   while(digitalRead(7)==HIGH){    //injos  
+   while(digitalRead(6)==HIGH){    //injos  && (ozpozitie>=-20 && ozpozitie<=8000)
     
-     if(ozpozitie>4500)
-        ozpozitie=4510;
+     if(ozpozitie>=3100)
+        ozpozitie=3101;
         
-     if(ozpozitie<=4500)
-        clockwise(),ozpozitie=ozpozitie+0.25;          
+     if(ozpozitie<=3100)
+        counterclockwise(),ozpozitie=ozpozitie+0.25;          
     }          
   
-   while(digitalRead(6)==HIGH){ 
+   while(digitalRead(7)==HIGH){ 
 
-      if(ozpozitie<-3000)
-        ozpozitie=-3010;
+      if(ozpozitie<=-3100)
+        ozpozitie=-3101;
     
-     if(ozpozitie>=-3000)
-       counterclockwise(),ozpozitie=ozpozitie-0.25;
+     if(ozpozitie>=-3100)
+       clockwise(),ozpozitie=ozpozitie-0.25;
 }
 }
+
 
 void repozitionareoz(){
    
    while(!(ozpozitie>=-1 && ozpozitie<=1)){
    if(ozpozitie>0)
-        counterclockwise(),ozpozitie=ozpozitie-0.25;
+       clockwise(),ozpozitie=ozpozitie-0.25;
 
    if(ozpozitie<0)
-         clockwise(),ozpozitie=ozpozitie+0.25;
+         counterclockwise(),ozpozitie=ozpozitie+0.25;
 }
 }
 
@@ -909,13 +1070,13 @@ void claw()
 void loop() {
 
   if(mfrc522.PICC_IsNewCardPresent() && credit==0 && start==0)       
-       credit=1, digitalWrite(35,HIGH), pag1();//cantecinceput();
+       credit=1, pag1(), play_rtttl(piesacredit);
   
   if(credit==1){
 
-   if(digitalRead(12)==HIGH && pag>1 && pressed==0)
+   if(digitalRead(13)==HIGH && pag>1 && pressed==0)
        pag--,switcher(),pressed=1,delay(300);
-   if(digitalRead(13)==HIGH && pag<14 && pressed==0)
+   if(digitalRead(12)==HIGH && pag<14 && pressed==0)
        pag++,switcher(),pressed=1,delay(300);
    
   if(digitalRead(12)==LOW && digitalRead(13)==LOW)
@@ -923,23 +1084,25 @@ void loop() {
 
 
    if(digitalRead(26)==HIGH)
-      start=1,credit=0,timp=120,lcd.clear(), obiectecastigate=0, deridicat=0, ajustaresenzorisimediesenzori(),timptrecut=millis();
+      start=1,credit=0,timp=/*1*/20,lcd.clear(), obiectecastigate=0, deridicat=0, ajustaresenzorisimediesenzori(), play_rtttl(piesaapasat),timptrecut=millis();
       
   }
 
   if(start==1){
      timer();
+     
      if(cadere()==1 && deridicat==0){
+        lcd.clear();
+        clearLEDs();
         deridicat=1;
         pagfelicitari();
-       // canteccadere();
-       obiectecastigate++;
+        obiectecastigate++;
      }
 
      if(cadere()==0 && deridicat==1)
         deridicat=0,lcd.clear();
 
-    if(analogRead(A0)>800 || analogRead(A1)>800 || analogRead(A0)<200 || analogRead(A1)<200)
+    while(analogRead(A0)>900 || analogRead(A1)>900 || analogRead(A0)<150 || analogRead(A1)<150)
       clearLEDs(),oxoy();
       
     if(digitalRead(7)==HIGH || digitalRead(6)==HIGH)
@@ -953,8 +1116,8 @@ void loop() {
       timptrecut=millis();
   }
 
-    if(timp==0)
-      start=0, digitalWrite(35,LOW), mesajfinal(), repozitionareoz();//cantecfinal();
+    if(timp<=0)
+      start=0,clearLEDs(), mesajfinal(), play_rtttl(piesafinal), repozitionarexoy(),repozitionareoz();
   }
 
   if(credit==0 && start==0)
